645212bb3f2d941ad50478197fc6f9f1babea127
EBOKPH <Kobeestabalaya@MacBookPro.local>
2023-05-12 00:27:12 +0800

    Add activity code

--- script.js
+++ script.js
@@ -1,55 +1,42 @@
-// Trainer object
-var trainer = {
+let trainer = {
   name: "Ash Ketchum",
   age: 10,
   pokemon: [],
   friends: {
-    Brock: ["Pikachu", "Onix"],
-    Misty: ["Staryu", "Starmie"]
+    brock: ["Pewter City", "Onix"],
+    misty: ["Cerulean City", "Starmie"]
   },
   talk: function() {
     console.log("Pikachu! I choose you!");
   }
 };
 
-// Pokemon constructor
+console.log(trainer.name);
+console.log(trainer.friends.brock[0]);
+
+trainer.talk();
+
 function Pokemon(name, level) {
   this.name = name;
   this.level = level;
   this.health = level * 10;
-  this.attack = level * 2;
-  this.tackle = function(target) {
-    target.health -= this.attack;
-    if (target.health <= 0) {
-      target.faint();
+  this.attack = level * 5;
+
+  this.tackle = function(targetPokemon) {
+    targetPokemon.health -= this.attack;
+    if (targetPokemon.health <= 0) {
+      this.faint(targetPokemon);
     }
   };
-  this.faint = function() {
-    console.log(this.name + " has fainted.");
+
+  this.faint = function(targetPokemon) {
+    console.log(`${targetPokemon.name} has fainted!`);
   };
 }
 
-// Instantiate Pokemon objects
-var pikachu = new Pokemon("Pikachu", 5);
-var charmander = new Pokemon("Charmander", 3);
-var bulbasaur = new Pokemon("Bulbasaur", 4);
-trainer.pokemon.push(pikachu, charmander, bulbasaur);
+let pikachu = new Pokemon("Pikachu", 5);
+let charmander = new Pokemon("Charmander", 4);
+let squirtle = new Pokemon("Squirtle", 3);
 
-// Update trainer info in HTML
-document.getElementById("trainer-name").textContent = trainer.name;
-document.getElementById("trainer-age").textContent = trainer.age;
-for (var i = 0; i < trainer.pokemon.length; i++) {
-  var li = document.createElement("li");
-  li.textContent = trainer.pokemon[i].name + " (Level " + trainer.pokemon[i].level + ")";
-  document.getElementById("trainer-pokemon").appendChild(li);
-}
-for (var friend in trainer.friends) {
-  var li = document.createElement("li");
-  li.textContent = friend + ": " + trainer.friends[friend].join(", ");
-  document.getElementById("trainer-friends").appendChild(li);
-}
-
-// Tackle function
-function attack() {
-  pikachu.tackle(charmander);
-}
+pikachu.tackle(charmander);
+console.log(charmander.health);
